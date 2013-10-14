0a47023837235b6eedc3d1744cf15bb1ea8a8403
nikmoon <consensuss@rambler.ru>
2013-10-14 17:17:39 +0700

    no comments

--- mymain.cpp
+++ mymain.cpp
@@ -29,13 +29,33 @@ int RunThreadMessageLoop()
 	return msg.wParam;
 }
 
-int main(int argc, char * argv[])
+
+int SeeThis()
 {
-//	CTestFrame * mainFrame = new CTestFrame(GetModuleHandle(NULL), "MyWindow");
-//	int res = RunThreadMessageLoop();
-//	delete mainFrame;
-//	return res;
-//	CTestFrame mainFrame = CTestFrame(GetModuleHandle(NULL), "MyWindow");
-//	return RunThreadMessageLoop();
-	return 0;
+	RECT rtClient = {0,0,600,300};
+	::AdjustWindowRect(&rtClient,WS_OVERLAPPEDWINDOW,FALSE);
+
+	CDialogFrame frameMain(GetModuleHandle(NULL),CS_HREDRAW|CS_VREDRAW,NULL,NULL,NULL,NULL,"MainFrame",WS_OVERLAPPEDWINDOW,0,
+			"App main window",NULL,100,100,rtClient.right - rtClient.left,rtClient.bottom - rtClient.top);
+
+	for(int y = 0; y < 300; y += 50)
+	{
+		for(int x = 0; x < 600; x += 100)
+		{
+			frameMain.AddButton("Кнопочка )))", x,y,100,50);
+		}
+	}
+
+	frameMain.ShowFrame();
+	frameMain.UpdateFrame();
+
+	return RunThreadMessageLoop();
+}
+
+
+int main(int argc, char **argv)
+{
+
+
+	return SeeThis();
 }
